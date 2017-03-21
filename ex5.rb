fb45c388b1d41c7b3ab1831ab5d3f9bec6ad97f9
JessSchoon <jess@Captain.local>
2017-03-21 15:24:45 -0700

    Me working through Ruby the hard way
    Proof to myself that I am learning!

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+name = 'Jess Schoon'
+age = 30
+height_inches = 66
+height_meters = height_inches * 0.0254
+weight_lbs = 160
+weight_kgs = weight_lbs * 0.453592
+eyes = 'Brown'
+teeth = 'white'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height_inches} inches tall and #{height_meters} meters tall."
+puts "He's #{weight_lbs} pounds heavy and #{weight_kgs} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height_inches}, and #{weight_lbs} I get #{age + height_inches + weight_lbs}."
